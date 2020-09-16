5e3a7017d26ff608b6aec5d77508ec5324eedbc1
comandpromt <vayase_a_ferrol@hotmail.com>
2020-09-16 16:57:16 +0200

    v 1.0

---
+++ Assets/Scripts/ControlPNJ.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPNJ : MonoBehaviour
+{
+	float speed;
+  
+    public float mana = 0f;
+  	
+    // Start is called before the first frame update
+    void Start()
+    { 
+        speed = 0.05f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(new Vector3(speed, 0,0));
+        }
+        
+		if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(new Vector3(-speed, 0, 0));
+        }
+		
+        if (Input.GetKey(KeyCode.UpArrow)) { 
+            transform.Translate(new Vector3(0, 0, speed));
+        }
+		
+        if (Input.GetKey(KeyCode.DownArrow)){
+
+            transform.Translate(new Vector3(0, 0,-speed));
+
+        }
+
+    }
+	
+	void OnCollisionEnter(Collision col) {
+ 
+        if (col.gameObject.tag=="col") {
+            Debug.Log("el pnj ha colisionado con el cubo");
+        }
+
+    }
+
+    void OnTriggerEnter(Collider obj) {
+     
+        if (obj.tag=="col") {
+            mana += 0.5f;
+        }
+
+    }
+	
+}
